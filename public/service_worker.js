2bb4a3dec6b5fdcd00108ff30273282f36097398
Kunal <gkunal13579@gmail.com>
2022-09-27 04:49:55 +0530

    main pwa try

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,51 +1,3 @@
-// const CACHE_NAME = 'sw-cache-example';
-// const toCache = [
-//   '/',
-//   '/index.html',
-// ];
-
-// self.addEventListener('install', function(event) {
-//   console.log('used to register the service worker');
-//   event.waitUntil(
-//     caches.open(CACHE_NAME)
-//       .then(function(cache) {
-//         return cache.addAll(toCache)
-//       })
-//       .then(self.skipWaiting())
-//   )
-// })
-
-// self.addEventListener('fetch', function(event) {
-//   console.log('used to intercept requests so we can check for the file or data in the cache')
-//   event.respondWith(
-//     fetch(event.request)
-//       .catch(() => {
-//         return caches.open(CACHE_NAME)
-//           .then((cache) => {
-//             return cache.match(event.request)
-//           })
-//       })
-//   )
-// })
-
-// self.addEventListener('activate', function(event) {
-//   console.log('this event triggers when the service worker activates');
-//   event.waitUntil(
-//     caches.keys()
-//       .then((keyList) => {
-//         return Promise.all(keyList.map((key) => {
-//           if (key !== CACHE_NAME) {
-//             console.log('[ServiceWorker] Removing old cache', key)
-//             return caches.delete(key)
-//           }
-//         }))
-//       })
-//       .then(() => self.clients.claim())
-//   )
-// })
-
-
-
 self.addEventListener("install", function(event) {
   event.waitUntil(preLoad());
 });
@@ -55,7 +7,7 @@ var preLoad = function(){
   return caches.open("offline").then(function(cache) {
     console.log("caching index and important routes");
     // return cache.addAll(["/blog/", "/blog", "/", "/contact", "/resume", "/offline.html"]);
-    return cache.addAll(["/offline.html"]);
+    return cache.addAll(["/index.html"]);
   });
 };
 
@@ -69,6 +21,7 @@ self.addEventListener("fetch", function(event) {
 var checkResponse = function(request){
   return new Promise(function(fulfill, reject) {
     fetch(request).then(function(response){
+      //console.log(response.status);
       if(response.status !== 404) {
         fulfill(response);
       } else {
@@ -91,7 +44,7 @@ var returnFromCache = function(request){
   return caches.open("offline").then(function (cache) {
     return cache.match(request).then(function (matching) {
      if(!matching || matching.status == 404) {
-       return cache.match("offline.html");
+       return cache.match("index.html");
      } else {
        return matching;
      }
